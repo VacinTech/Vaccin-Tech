8d19a767198e87ff9962705882559fb44b730e0a
Vitor Canquerini <titolucas1616@gmail.com>
2026-06-04 13:45:32 -0300

    updt

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE VaccinTech;
 USE VaccinTech;
 
+
 CREATE TABLE Empresa (
     idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
     razaoSocial VARCHAR(100),
@@ -8,8 +9,9 @@ CREATE TABLE Empresa (
     telefone VARCHAR(15)
 );
 
+
 CREATE TABLE Usuario (
-    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT auto_increment,
     nomeCompleto VARCHAR(100),
     email VARCHAR(80) UNIQUE,
     senha VARCHAR(50),
@@ -19,6 +21,8 @@ CREATE TABLE Usuario (
     CONSTRAINT chkPerfil CHECK (perfil IN ('Admin', 'Gerente', 'Operário')),
     CONSTRAINT fkUserEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
+
+
 
 CREATE TABLE Transporte (
     idTransporte INT PRIMARY KEY AUTO_INCREMENT,
@@ -36,7 +40,7 @@ CREATE TABLE Vacina (
     nome VARCHAR(100),
     fabricante VARCHAR(100),
     lote VARCHAR(50),
-    temperaturaMin DECIMAL(4,2) DEFAULT 2.00, -- Padronizado conforme ANVISA [7]
+    temperaturaMin DECIMAL(4,2) DEFAULT 2.00, 
     temperaturaMax DECIMAL(4,2) DEFAULT 8.00,
     fkTransporte INT,
     CONSTRAINT fkVacinaTransp FOREIGN KEY (fkTransporte) REFERENCES Transporte(idTransporte)
@@ -47,7 +51,7 @@ CREATE TABLE Sensor (
     idSensor INT PRIMARY KEY AUTO_INCREMENT,
     modelo VARCHAR(50) DEFAULT 'LM35',
     dataInstalacao DATE,
-    fkTransporte INT,
+    fkTransporte INT UNIQUE,
     CONSTRAINT fkSensorTransp FOREIGN KEY (fkTransporte) REFERENCES Transporte(idTransporte)
 );
 
@@ -55,21 +59,54 @@ CREATE TABLE Monitoramento (
     idMonitoramento INT PRIMARY KEY AUTO_INCREMENT,
     temperatura DECIMAL(5,2),
     dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fkSensor INT,
+    fkSensor INT NOT NULL,
     CONSTRAINT fkDadoSensor FOREIGN KEY (fkSensor) REFERENCES Sensor(idSensor)
 );
 
 
 CREATE TABLE Alerta (
-    idAlerta INT,
-    fkMonitoramento INT,
+    idAlerta INT PRIMARY KEY,
+    fkMonitoramento INT NOT NULL UNIQUE,
     tipoAlerta VARCHAR(50),
     dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
     confirmacaoLeitura BOOLEAN DEFAULT FALSE,
-    fkUsuarioConfirmacao INT,
-    PRIMARY KEY (idAlerta, fkMonitoramento),
     CONSTRAINT fkAlertaMonitor FOREIGN KEY (fkMonitoramento) 
-        REFERENCES Monitoramento(idMonitoramento),
-    CONSTRAINT fkAlertaUser FOREIGN KEY (fkUsuarioConfirmacao) 
-        REFERENCES Usuario(idUsuario)
+        REFERENCES Monitoramento(idMonitoramento)
 );
+
+-- select da ultima temperatura
+-- É o mesmo para a KPI de faixa atual
+select temperatura 
+	from monitoramento
+    where fkSensor = '${fkSensor}'
+    order by idMonitoramento desc;
+    
+-- view de min e max
+create view vwMinEMaxSensor as
+select fkSensor, min(temperatura) as tempMin, max(temperatura) as tempMax
+	from monitoramento
+		group by fkSensor;
+    
+select * from vwMinEMaxSensor 
+	where fkSensor = '${fkSensor}';
+
+
+-- VIEW que traz tudo 
+create view vwMonitoramentoCompleto as
+select
+    e.razaoSocial,
+    t.placa,
+    s.idSensor,
+    m.temperatura,
+    m.dataHora
+from Monitoramento m
+join Sensor s on m.fkSensor = s.idSensor
+join Transporte t on s.fkTransporte = t.idTransporte
+join Empresa e on t.fkEmpresa = e.idEmpresa;
+
+-- Tem que usar a VIEW assim para puxar o relatorio completo de apenas um sensor
+select * 
+	from vwMonitoramentoCompleto
+	where idSensor = '${idSensor}';
+    
+    
