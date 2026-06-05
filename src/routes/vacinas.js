b6a2f3636a452ef1c3bd3296ec824b66a44c6c71
ViniciusMartins01 <vinicius.moliveira@sptech.school>
2026-06-05 15:53:22 -0300

    Create vacinas.js

---
+++ src/routes/vacinas.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+var vacinaController = require("../controllers/vacinaController");
+
+router.post("/cadastrar", function (req, res) {
+    vacinaController.cadastrar(req, res);
+})
+
+router.post("/listar", function (req, res) {
+    vacinaController.listar(req, res);
+});
+
+
+module.exports = router;
