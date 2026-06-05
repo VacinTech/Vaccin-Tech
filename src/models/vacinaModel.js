d53b783307246f87f8f80e7c7787d05cd68707f9
ViniciusMartins01 <vinicius.moliveira@sptech.school>
2026-06-05 20:24:47 -0300

    add backend
    add conexao dos cadastros com o bd

--- src/models/vacinaModel.js
+++ src/models/vacinaModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function listar() {
     console.log("ACESSEI O VACINA  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucaoSql = `
-        SELECT idVacina, nome, fabricante, lote, tempMin, tempMax, dataValidade, dataFabricacao FROM vacina;`;
+        SELECT idVacina, nome, fabricante, lote, temperaturaMin, temperaturaMax, dtValidade, dtFabricacao FROM vacina;`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
