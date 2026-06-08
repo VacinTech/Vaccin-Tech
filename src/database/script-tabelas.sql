e859a8b1e892074c38fc759de838cf3201aa00e8
Vitor Canquerini <titolucas1616@gmail.com>
2026-06-08 13:21:47 -0300

    Update script-tabelas.sql

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE VaccinTech;
 USE VaccinTech;
 
-CREATE TABLE empresa (
+
+
+CREATE TABLE Empresa (
     idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
     razaoSocial VARCHAR(100),
     cnpj CHAR(14) UNIQUE,
@@ -9,7 +11,7 @@ CREATE TABLE empresa (
 );
 
 
-CREATE TABLE usuario (
+CREATE TABLE Usuario (
     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nomeCompleto VARCHAR(100),
     email VARCHAR(80) UNIQUE,
@@ -21,9 +23,13 @@ CREATE TABLE usuario (
     CONSTRAINT fkUserEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
+select * from Usuario;
 
 
-CREATE TABLE transporte (
+
+
+
+CREATE TABLE Transporte (
     idTransporte INT PRIMARY KEY AUTO_INCREMENT,
     placa VARCHAR(10) UNIQUE not null,
     modelo VARCHAR(50) not null,
@@ -32,19 +38,16 @@ CREATE TABLE transporte (
     CONSTRAINT fkTranspEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
-CREATE TABLE vacina (
+CREATE TABLE Vacina (
     idVacina INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100),
     fabricante VARCHAR(100),
     lote VARCHAR(50),
-    dtValidade date not null,
-    dtFabricacao date not null,
     temperaturaMin DECIMAL(4,2) DEFAULT 2.00, 
-    temperaturaMax DECIMAL(4,2) DEFAULT 8.00,
-    dtCadastro datetime default current_timestamp
+    temperaturaMax DECIMAL(4,2) DEFAULT 8.00
 );
 
-create table viagem (
+create table Viagem (
 idViagem int auto_increment,
 fkVacina int not null,
 fkTransporte int not null,
@@ -55,15 +58,14 @@ qtdVacina int,
 statusViagem VARCHAR(30) DEFAULT 'Trânsito',
 constraint pkViagem primary key (idViagem, fkVacina, fkTransporte),
 constraint fkVacinaViagem foreign key (fkVacina) 
-	references vacina(idVacina),
+	references Vacina(idVacina),
 constraint fkTransporteViagem foreign key (fkTransporte)
-	references transporte(idTransporte),
+	references Transporte(idTransporte),
     CONSTRAINT chkStatus CHECK (statusViagem IN ('Trânsito', 'Concluída', 'Cancelada'))
-); 
+);
 
 
-
-CREATE TABLE sensor (
+CREATE TABLE Sensor (
     idSensor INT PRIMARY KEY AUTO_INCREMENT,
     modelo VARCHAR(50) DEFAULT 'LM35',
     dataInstalacao DATE,
@@ -71,16 +73,29 @@ CREATE TABLE sensor (
     CONSTRAINT fkSensorTransp FOREIGN KEY (fkTransporte) REFERENCES Transporte(idTransporte)
 );
 
-CREATE TABLE monitoramento (
-    idMonitoramento INT PRIMARY KEY AUTO_INCREMENT,
-    temperatura DECIMAL(5,2),
-    dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fkSensor INT NOT NULL,
-    CONSTRAINT fkDadoSensor FOREIGN KEY (fkSensor) REFERENCES Sensor(idSensor)
+create table Monitoramento (
+    idMonitoramento int primary key auto_increment,
+    temperatura decimal(5,2),
+    dataHora datetime default current_timestamp,
+    fkSensor int not null,
+    constraint fkSensorCon
+        foreign key (fkSensor)
+        references Sensor(idSensor)
 );
 
+select DATE_FORMAT(dataHora,'%H:%i:%s') from Monitoramento where idMonitoramento = 1;
+select dataHora from monitoramento where idMonitoramento = 1;
 
-CREATE TABLE alerta (
+
+SELECT 
+                        temperatura,
+                        dataHora,
+                        date_format(dataHora,'%H:%i:%s') as hora
+                    FROM Monitoramento
+                    WHERE fkSensor = 2
+                    ORDER BY idMonitoramento DESC LIMIT 2;
+                    
+CREATE TABLE Alerta (
     idAlerta INT,
     fkMonitoramento INT NOT NULL UNIQUE,
     tipoAlerta VARCHAR(50),
@@ -90,14 +105,14 @@ CREATE TABLE alerta (
         REFERENCES Monitoramento(idMonitoramento)
 );
 
-insert into empresa (razaoSocial, cnpj, telefone) values
+insert into Empresa (razaoSocial, cnpj, telefone) values
 ('Pfizer Brasil Ltda', '12345678000101', '1130011001'),
 ('BioNTech Logistica S.A.', '12345678000102', '1130011002'),
 ('Vacina Express Transportes', '12345678000103', '1130011003'),
 ('HealthCargo Distribuicao', '12345678000104', '1130011004'),
 ('ImunoTech Solutions', '12345678000105', '1130011005');
 
-insert into usuario values
+insert into Usuario values
 (default, 'Tito', 'tito@gmail.com', '12345678', 'Admin', '12212212212', 1);
 
 
@@ -120,13 +135,14 @@ create view vwGeralRotas as
 	order by m.idMonitoramento desc
 	limit 1
     ) as temperaturaAtual
-		from viagem vi
-        join transporte tr on vi.fkTransporte = tr.idTransporte
-        join vacina v on vi.fkVacina = v.idVacina
-        join sensor s on s.fkTransporte = tr.idTransporte
+		from Viagem vi
+        join Transporte tr on vi.fkTransporte = tr.idTransporte
+        join Vacina v on vi.fkVacina = v.idVacina
+        join Sensor s on s.fkTransporte = tr.idTransporte
         where vi.statusViagem = 'Trânsito';
             
-            
+
+
 select * from vwGeralRotas
 	 where fkEmpresa = 1; -- aqui coloca ${idEmpresa};
      
@@ -147,9 +163,9 @@ create view vwTudoDashIndividual as
 			order by m.idMonitoramento desc
 			limit 1
 		) as temperaturaAtual
-			from viagem vi
-			join transporte tr on vi.fkTransporte = tr.idTransporte
-			join vacina v on vi.fkVacina = v.idVacina
+			from Viagem vi
+			join Transporte tr on vi.fkTransporte = tr.idTransporte
+			join Vacina v on vi.fkVacina = v.idVacina
             left join (
             select 
 				s.fkTransporte,
@@ -166,7 +182,8 @@ select * from vwTudoDashIndividual
 
 
 select temperatura	
-	from monitoramento where fkSensor = 1;
+	from Monitoramento where fkSensor = 1;
+    
 -- VACINAS
 INSERT INTO Vacina (nome, fabricante, lote) VALUES
 ('Comirnaty', 'Pfizer', 'PF2026001'),
@@ -214,15 +231,12 @@ INSERT INTO Monitoramento (temperatura, fkSensor) VALUES
 (5.1, 1),
 (5.3, 1),
 (5.4, 1),
-
 (6.0, 2),
 (6.1, 2),
 (6.2, 2),
-
 (4.6, 3),
 (4.7, 3),
 (4.8, 3),
-
 (6.8, 4),
 (6.9, 4),
 (7.1, 4);
@@ -246,6 +260,10 @@ VALUES
 (1, 3, 'Temperatura Alta'),
 (2, 6, 'Temperatura Alta');
 
-select * from usuario;
+select * from Usuario;
+select * from Viagem;
+select * from Monitoramento;
+insert into Monitoramento (temperatura, fkSensor) values
+(8.0, 1);
 
- SELECT idUsuario, nomeCompleto, email, perfil, fkEmpresa FROM usuario WHERE email = 'tito@gmail.com' AND senha = '12345678'
+ SELECT idUsuario, nomeCompleto, email, perfil, fkEmpresa FROM usuario WHERE email = 'tito@gmail.com' AND senha = '12345678';
