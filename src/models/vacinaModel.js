1ae7bc4b98558beecfbddf4d7c4bf9d712e41be0
ViniciusMartins01 <vinicius.moliveira@sptech.school>
2026-06-08 17:58:44 -0300

    Update vacinaModel.js

--- src/models/vacinaModel.js
+++ src/models/vacinaModel.js
@@ -3,14 +3,14 @@ var database = require("../database/config");
 function listar() {
     console.log("ACESSEI O VACINA  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucaoSql = `
-        SELECT idVacina, nome, fabricante, lote, temperaturaMin, temperaturaMax, dtValidade, dtFabricacao FROM vacina;`;
+        SELECT idVacina, nome, fabricante, lote, temperaturaMin, temperaturaMax, dtValidade, dtFabricacao FROM Vacina;`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function cadastrar(nome, fabricante, lote, tempMin, tempMax, dataValidade, dataFabricacao, idUsuarioVar) {
     console.log("ACESSEI O VACINA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar(): ", nome, fabricante, lote, tempMin, tempMax, dataValidade, dataFabricacao, idUsuarioVar);
-    var instrucaoSql = `insert into vacina (nome, fabricante, lote, temperaturaMin, temperaturaMax, dtValidade, dtFabricacao, fkUsuario) values ('${nome}', '${fabricante}', '${lote}', ${tempMin}, ${tempMax}, '${dataValidade}', '${dataFabricacao}', ${idUsuarioVar});`;
+    var instrucaoSql = `insert into Vacina (nome, fabricante, lote, temperaturaMin, temperaturaMax, dtValidade, dtFabricacao, fkUsuario) values ('${nome}', '${fabricante}', '${lote}', ${tempMin}, ${tempMax}, '${dataValidade}', '${dataFabricacao}', ${idUsuarioVar});`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
